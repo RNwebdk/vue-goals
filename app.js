cc7479e757d11ae1de116e20ea5dfa8b6c46104a
Rami <cashamir@gmail.com>
2020-11-03 00:52:57 +0100

    learning about conditional content and lists

--- app.js
+++ app.js
@@ -1,26 +1,7 @@
 const app = Vue.createApp({
-	data(){
-		return {
-			className: '',
-			toggle: true,
-			inlineBackgroundColor: ''
-		}
-	},
-	computed: {
-		paraclasses(){
-			return {
-				user1: this.className === 'user1',
-				user2: this.className === 'user2',
-				visible: this.toggle,
-				hidden: !this.toggle
-			}
-		}
-	},
-	methods: {
-		toggleMe(){
-			this.toggle = !this.toggle;
-		}
-	}
+  data() {
+    return { goals: [] };
+  },
 });
 
-app.mount('#assignment');
+app.mount('#user-goals');
